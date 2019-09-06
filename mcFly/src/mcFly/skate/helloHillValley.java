1c515fbf2855e69431ea8cde7dc42a52a94bc0ea
zechariah.roberson <zechariah.roberson@MXL3250J0F.DRMAIN.local>
2019-09-06 13:40:07 -0400

    Adding a mcfly to hill valley

---
+++ mcFly/src/mcFly/skate/helloHillValley.java
@@ -0,0 +1,8 @@
+package mcFly.skate;
+
+public class helloHillValley {
+	public static void main(String[] args) {
+		System.out.println("Hello Hill Valley!");
+		System.out.println("Re-elect Mayor Goldie Wilson!");
+	}
+}
